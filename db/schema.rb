a3e331ede49c9bd79aef38be55eaf6023b2f27ee
edynak-demo <edynakdemo@gmail.com>
2020-05-11 16:48:39 -0400

    implemented room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_184202) do
+ActiveRecord::Schema.define(version: 2020_05_11_203932) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "home_type"
+    t.string "room_type"
+    t.integer "accommodate"
+    t.integer "bed_room"
+    t.integer "bath_room"
+    t.string "listing_name"
+    t.text "summary"
+    t.string "address"
+    t.boolean "is_tv"
+    t.boolean "is_kitchen"
+    t.boolean "is_dishwasher"
+    t.boolean "is_air_conditioning"
+    t.boolean "is_heating"
+    t.boolean "is_internet"
+    t.boolean "is_washing_machine"
+    t.boolean "is_dryer"
+    t.boolean "is_toiletries_provided"
+    t.boolean "is_parking"
+    t.boolean "is_doorman"
+    t.boolean "is_elevator"
+    t.integer "price"
+    t.boolean "active"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +63,5 @@ ActiveRecord::Schema.define(version: 2020_05_11_184202) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
